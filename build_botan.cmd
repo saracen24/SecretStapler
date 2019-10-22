cec1d68926c6831c5bbd0622f59da0669fc9734b
saracen24 <oppressor24@gmail.com>
2019-10-23 01:15:45 +0300

    Add disable flags to botan build script.

--- build_botan.cmd
+++ build_botan.cmd
@@ -9,7 +9,7 @@ call "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/Tool
 call "%VS140COMNTOOLS%../../VC/vcvarsall.bat" x64
 
 :: Configure
-python configure.py --with-cmake
+python configure.py --with-cmake --without-documentation --without-sphinx --without-pdf --without-rst2man --without-doxygen
 
 pushd "build"
 :: Configure and build
